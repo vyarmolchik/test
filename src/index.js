29ccd9835689ce2fcdb27055f86c2f8207a2b2ac
VYarmolchik <walka972304>
2017-04-18 12:51:39 +0300

    Create base structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var App = function() {
+    //  console.log('App init');
+};
+
+var app = new App();
+app.get = function() {};
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    }
+};
